c78ea6fd580fd3a915937af6bcd62b27e6542d9c
Lekan Ojulowo <lekanojulowo@gmail.com>
2020-01-21 00:25:18 +0100

    More Update
    More update, having some errors though

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
+function Todo({todo, index}){
+return <div className="todo">{todo.task}</div>;
+}
+
+function TodoForm({addTodo}){
+  const [value, setValue] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if(!value) return;
+    addTodo(value);
+    setValue('');
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" className="input" placeholder="type new todo here"  value={value} onChange={e => setValue(e.target.value)} />
+      <input type="submit" value="Add Task" />
+    </form>
+  )
+}
+
 function App() {
+const [todos, setTodos] = useState([
+  {
+    task: 'Learn about React',
+    isCompleted:false
+  },
+  {
+    task: 'Take a rest',
+    isCompleted:true
+  },
+  {
+    task: 'Build really nice todo app with react',
+    isCompleted:false
+  }
+])
+
+const addTodo = task => {
+  const NewTodos = {...todos, task};
+  setTodos(NewTodos);
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>
+       
+        <h1 className="todo-title">
           Exquis Todo App 
         </h1>
-        <ul>
-          <li>Study JavaScript</li>
-          <li>Go Gym</li>
-          <li>Take time to pray</li>
-          <li>Learn to appreciate</li>
-        </ul>
-      </header>
+        <div className="todo-list">
+           {todos.map((todo, index) => <Todo key={index} index={index} todo={todo} />)}
+        </div> 
+        <TodoForm addTodo={addTodo} />
     </div>
   );
 }
